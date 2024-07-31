7c3d642353cbe6d3b8c626d8d13633e838161814
ishaa-jiit <is0157650@gmail.com>
2024-07-31 15:53:43 +0530

    Add files via upload

---
+++ week1/week1b/q3.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    string s, temp;
+    map<string, int> mp;
+    cout << "Enter para:" << endl;
+    getline(cin, s);
+    vector<string> words;
+    int start = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            temp = s.substr(start, i - start);
+            mp[temp]++;
+            start = i + 1;
+        }
+    }
+    temp = s.substr(start, s.length() - start);
+    mp[temp]++;
+    map<string, int>::iterator it = mp.begin();
+    cout << endl;
+    cout << "Word -> Frequency:" << endl;
+    while (it != mp.end())
+    {
+        cout << it->first << ": " << it->second << endl;
+        ++it;
+    }
+    cout << endl;
+    return 0;
+}
